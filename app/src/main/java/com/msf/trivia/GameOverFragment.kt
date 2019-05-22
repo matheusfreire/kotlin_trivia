7ea5dcc655e1a8caa21a9d1e4b3f996be3c596e0
matheusfreire <matheus.sfreire@gmail.com>
2019-05-22 16:53:43 -0300

    Add game over fragment

---
+++ app/src/main/java/com/msf/trivia/GameOverFragment.kt
@@ -0,0 +1,22 @@
+package com.msf.trivia
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class GameOverFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_game_over, container, false)
+    }
+
+
+}
