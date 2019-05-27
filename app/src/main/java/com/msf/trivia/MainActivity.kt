01549e82b218a8348e9b98f4ed1b68c2f28926be
matheusfreire <matheus.sfreire@gmail.com>
2019-05-27 17:57:57 -0300

    Create  up navigation

--- app/src/main/java/com/msf/trivia/MainActivity.kt
+++ app/src/main/java/com/msf/trivia/MainActivity.kt
@@ -3,6 +3,8 @@ package com.msf.trivia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.msf.trivia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val dataBinding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = findNavController(R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
     }
 }
