c9259d977348e3221328faf00c95d56d626bc7aa
matheusfreire <matheus.sfreire@gmail.com>
2019-05-30 18:49:29 -0300

    Animation

--- app/src/main/java/com/msf/trivia/MainActivity.kt
+++ app/src/main/java/com/msf/trivia/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.msf.trivia.databinding.ActivityMainBinding
@@ -18,6 +20,13 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.navHostFragment)
         drawerLayout = dataBinding.drawerLayout
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, arguments ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
         NavigationUI.setupWithNavController(dataBinding.navView, navController)
     }
 
